76795fcbfdcdf131b8b6cef17143abcd4bc37758
James Stamps Jr <jstampshere@gmail.com>
2020-04-22 14:35:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-    if array.include?(cheddar) {|x| x == "cheese_types"}
+    if array.include?("cheddar") {|x| x == "cheese_types"}
     end
 end
 
