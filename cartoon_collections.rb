24d67a6a1e2d463013042e488f37c02150c62fef
James Stamps Jr <jstampshere@gmail.com>
2020-04-22 15:03:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,13 +27,20 @@ def find_the_cheese(array)
   end
 end
 =end
+=begin
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese|
     cheese_types.include?(array)
  end
 end
-
+=end
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|
+    cheese_types.include?(cheese)
+  end
+end
 
 =begin
 def find_the_cheese(array)
