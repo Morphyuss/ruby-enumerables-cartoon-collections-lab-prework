eb8e07e4915c3bfcf82f93edc172749a9e52c2cc
James Stamps Jr <jstampshere@gmail.com>
2020-04-22 15:00:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,13 +19,21 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
     array.include?("cheddar")
 end
-
+=begin
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.each do |string|
-     cheese_types.include?("string") == "cheddar"
+     cheese_types.include?("string")
   end
 end
+=end
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese| 
+    cheese_types.include?(array)
+ end
+end
+
 
 =begin
 def find_the_cheese(array)
