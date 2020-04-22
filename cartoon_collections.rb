8ffa7d0d73d62e8a3d3272ba335652a080040985
James Stamps Jr <jstampshere@gmail.com>
2020-04-22 14:21:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-    if array.include? {|x| x == "cheddar"}
+    if array.include?("cheddar") {|x| x == "cheese_types[0]"}
     end
 end
 
