90bf2237f0584b99f198676a69fbe845e89524f1
James Stamps Jr <jstampshere@gmail.com>
2020-04-22 15:07:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,3 +21,16 @@ def find_the_cheese(array)
     cheese_types.include?(cheese)
   end
 end
+Cartoon Collections
+  #roll_call_dwarves
+    prints out the 7 dwarfs in a numbered list
+  #summon_captain_planet
+    returns an array with the same number of elements that it was given
+    capitalizes each element and adds an exclamation mark (test 1)
+    capitalizes each element and adds an exclamation mark (test 2)
+    #long_planeteer_calls
+        returns true if any calls are longer than 4 characters
+        returns false if all calls are 4 characters or less
+      #find_the_cheese
+        returns the first element of the array that is cheese
+        returns nil if the array does not contain a type of cheese
